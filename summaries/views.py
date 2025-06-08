26af9f00286cad84d237a1c88991d3dd94ea316b
dedecode <andredeco.dev@gmail.com>
2025-06-08 19:47:47 -0300

    Git emergencial sem testes, muitas adições para redis pubsub

--- summaries/views.py
+++ summaries/views.py
@@ -1,13 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import DailySummary, WeeklySummary
 from .serializers import DailySummarySerializer, WeeklySummarySerializer
-from rest_framework.permissions import IsAuthenticated
+from core.utils.pubsub import publish_event
 
 class DailySummaryViewSet(viewsets.ModelViewSet):
-    """
-    API para gerenciar resumos diários do usuário autenticado.
-    Permite criar, visualizar, atualizar e deletar resumos por data.
-    """
     serializer_class = DailySummarySerializer
     permission_classes = [IsAuthenticated]
 
@@ -15,14 +12,24 @@ class DailySummaryViewSet(viewsets.ModelViewSet):
         return DailySummary.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        instance = serializer.save(user=self.request.user)
+        publish_event("summary_channel", {
+            "type": "daily",
+            "id": instance.id,
+            "title": instance.title,
+            "created_by": self.request.user.username
+        })
 
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        publish_event("summary_channel", {
+            "type": "daily",
+            "id": instance.id,
+            "title": instance.title,
+            "edited_by": self.request.user.username
+        })
 
 class WeeklySummaryViewSet(viewsets.ModelViewSet):
-    """
-    API para gerenciar resumos semanais do usuário autenticado.
-    Permite criar, visualizar, atualizar e deletar resumos por semana.
-    """
     serializer_class = WeeklySummarySerializer
     permission_classes = [IsAuthenticated]
 
@@ -30,5 +37,19 @@ class WeeklySummaryViewSet(viewsets.ModelViewSet):
         return WeeklySummary.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        instance = serializer.save(user=self.request.user)
+        publish_event("summary_channel", {
+            "type": "weekly",
+            "id": instance.id,
+            "title": instance.title,
+            "created_by": self.request.user.username
+        })
 
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        publish_event("summary_channel", {
+            "type": "weekly",
+            "id": instance.id,
+            "title": instance.title,
+            "edited_by": self.request.user.username
+        })
